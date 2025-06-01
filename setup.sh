5b9baa0be804e18e380bf741c5bd20f1f5e22b93
Donald Sebastian Leung <donaldsebleung@gmail.com>
2025-06-01 12:09:39 +0000

    Add dummy workflow for now as placeholder

--- setup.sh
+++ setup.sh
@@ -17,3 +17,20 @@ kubectl -n minio wait --for=condition=Available deploy --all --timeout=300s
 kubectl apply -k minio/
 echo "Waiting for bucket and data to be available, this may take up to 5m0s ..."
 kubectl -n minio wait --for=condition=Complete jobs --all --timeout=300s
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm -n argo install \
+	-f argo-workflows/values.yaml \
+	argo-workflows \
+	argo/argo-workflows \
+	--version 0.45.15 \
+	--create-namespace
+echo "Waiting for Argo Workflows controller and server to be available, this may take up to 5m0s ..."
+kubectl -n argo wait --for=condition=Available deploy --all --timeout=300s
+kubectl -n argo create -f argo-workflows/workflowtemplates.yaml
+echo "Waiting for 5s ..."
+sleep 5
+kubectl -n argo create -f argo-workflows/workflows.yaml
+echo "Waiting for workflow to complete, this may take up to 5m0s ..."
+kubectl -n argo wait --for=condition=Completed workflows.argoproj.io --all --timeout=300s
